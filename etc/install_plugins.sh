c5a36d09e1ca97dfd7b0812ba068526c65d4cac5
Jason <git-admin@casjaysdev.com>
2022-07-09 16:19:59 -0400

    🗃️  modified: etc/install_plugins.sh 🗃️

--- etc/install_plugins.sh
+++ etc/install_plugins.sh
@@ -1,31 +1,33 @@
 #!/usr/bin/env bash
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version           :  202207042123-git
+# @Author            :  Jason Hempstead
+# @Contact           :  jason@casjaysdev.com
+# @License           :  LICENSE.md
+# @ReadME            :  install_plugins --help
+# @Copyright         :  Copyright: (c) 2022 Jason Hempstead, Casjays Developments
+# @Created           :  Saturday, Jul 09, 2022 15:22 EDT
+# @File              :  install_plugins
+# @Description       :
+# @TODO              :
+# @Other             :
+# @Resource          :
+# @sudo/root         :  no
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 APPNAME="$(basename "$0")"
-VERSION="202108242343-git"
-USER="${SUDO_USER:-${USER}}"
-HOME="${USER_HOME:-${HOME}}"
+VERSION="202207042123-git"
+HOME="${USER_HOME:-$HOME}"
+USER="${SUDO_USER:-$USER}"
+RUN_USER="${SUDO_USER:-$USER}"
 SRC_DIR="${BASH_SOURCE%/*}"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set bash options
-[[ "$1" == "--debug" ]] && shift 1 && set -euo pipefail
+if [[ "$1" == "--debug" ]]; then shift 1 && set -xo pipefail && export SCRIPT_OPTS="--debug" && export _DEBUG="on"; fi
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version       : 202108242343-git
-# @Author        : Jason Hempstead
-# @Contact       : jason@casjaysdev.com
-# @License       : WTFPL
-# @ReadME        : install_plugins.sh
-# @Copyright     : Copyright: (c) 2021 Jason Hempstead, CasjaysDev
-# @Created       : Tuesday, Aug 24, 2021 23:43 EDT
-# @File          : install_plugins.sh
-# @Description   : Install tmux plugins
-# @TODO          :
-# @Other         :
-# @Resource      :
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TMUX_HOME="$HOME/.config/tmux"
-TMUX_SHARE_DIR="$HOME/.local/share/tmux"
-TMUX_PLUGIN_MANAGER_PATH="$TMUX_SHARE_DIR/tpm"
+TMUX_HOME="${TMUX_HOME:-$HOME/.config/tmux}"
+TMUX_SHARE_DIR="${TMUX_SHARE_DIR:-$HOME/.local/share/tmux}"
+TMUX_PLUGIN_MANAGER_PATH="${TMUX_PLUGIN_MANAGER_PATH:-$TMUX_SHARE_DIR/tpm}"
 export TMUX_HOME TMUX_PLUGIN_MANAGER_PATH TMUX_SHARE_DIR
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 [[ -d "$TMUX_PLUGIN_MANAGER_PATH/.git" ]] && git -C "$TMUX_PLUGIN_MANAGER_PATH" pull ||
@@ -35,8 +37,10 @@ export TMUX_HOME TMUX_PLUGIN_MANAGER_PATH TMUX_SHARE_DIR
   $TMUX_PLUGIN_MANAGER_PATH/bin/install_plugins
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 mkdir -p "$TMUX_SHARE_DIR/resurrect"
-[[ -f "$TMUX_HOME/resurrect" ]] && ln -sf "$TMUX_HOME/resurrect" "$TMUX_SHARE_DIR/resurrect/last"
+[[ -f "$TMUX_HOME/resurrect" ]] &&
+  ln -sf "$TMUX_HOME/resurrect" "$TMUX_SHARE_DIR/resurrect/last"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-[[ -f "$TMUX_PLUGIN_MANAGER_PATH/tpm" ]] && [[ -f "$TMUX_SHARE_DIR/resurrect/last" ]] && echo "Install completed"
+[[ -f "$TMUX_PLUGIN_MANAGER_PATH/tpm" ]] && [[ -f "$TMUX_SHARE_DIR/resurrect/last" ]] &&
+  echo "Install completed"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 exit ${exitCode:-$?}
